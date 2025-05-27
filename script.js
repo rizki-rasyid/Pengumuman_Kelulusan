4420f8629756caa850d096b28de6f84fb64f98d1
Rizki R. Rasyid <rizkiranda.rasyid@gmail.com>
2025-05-28 00:03:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ form.addEventListener("submit", function(event) {
     if (dataSiswa[nomorUjian]) {
         const siswa = dataSiswa[nomorUjian];
         hasil.innerHTML = `
-            <img src:"assets/foto_siswa/${siswa.foto}" alt="Foto ${siswa.nama}">
+            <img src:"assets/nama_siswa/${siswa.foto}" alt="Foto ${siswa.nama}">
             <p>Nama: <strong>${siswa.nama}</strong></p>
             <p>Status Kelulusan: <strong style="color: ${siswa.status === 'LULUS' ? 'green' : 'red'}">${siswa.status}</strong></p>
             <p style="color: #444">Selamat ${siswa.nama} ! Kamu dinyatakan lulus</p>
